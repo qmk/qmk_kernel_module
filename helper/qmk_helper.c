a97b0f856ed3995606d81cf65cb55c05802914a2
Jack Humbert <jack.humb@gmail.com>
2019-11-07 15:53:19 +0000

    handle mods correctly

--- helper/qmk_helper.c
+++ helper/qmk_helper.c
@@ -9,6 +9,7 @@
 // #include <ncurses.h>
 
 #include <qmk/keycodes/strings.h>
+#include <qmk/keycodes/basic.h>
 #include "qmk_gadget.h"
 #include "qmk_socket.h"
 #include "qmk_socket_listener.h"
@@ -25,6 +26,7 @@
 
 static int sig_flag = 1;
 static bool keys_down[256] = { 0 };
+static uint8_t mods = 0;
 
 void interrupt_signal(int sig)
 {
@@ -51,73 +53,99 @@ void send_test()
 
 void handle_message(char *msg)
 {
-    int i;
+	int i;
 
-    if (msg[0] == HANDSHAKE) {
-        msg++;
-        printf("\033[0;33m%s\033[0m\n", msg);
-    } else if (msg[0] == MSG_GENERIC) {
-        msg++;
-        printf("\033[0;33m%s\033[0m\n", msg);
-    } else if (msg[0] == KEYCODE_HID) {
-        msg++;
-        uint8_t ch = (uint8_t)msg[0];
-        bool pressed = (bool)msg[1];
+	if (msg[0] == HANDSHAKE) {
+		msg++;
+		printf("\033[0;33m%s\033[0m\n", msg);
+	} else if (msg[0] == MSG_GENERIC) {
+		msg++;
+		printf("\033[0;33m%s\033[0m\n", msg);
+	} else if (msg[0] == KEYCODE_HID) {
+		msg++;
+		uint8_t ch = (uint8_t)msg[0];
+		bool pressed = (bool)msg[1];
 
-        if (pressed) {
-            printf("\033[1;32mHID keycode pressed:  (0x%.2X) %s\033[0m\n",
-                   ch, keycode_to_string[ch]);
-        } else {
-            printf("\033[0;32mHID keycode released: (0x%.2X) %s\033[0m\n",
-                   ch, keycode_to_string[ch]);
-        }
-    } else if (msg[0] == MATRIX_EVENT) {
-        msg++;
-        uint8_t row = (uint8_t)msg[0] - 1;
-        uint8_t col = (uint8_t)msg[1] - 1;
-        bool pressed = (bool)msg[2];
+		if (pressed) {
+			printf("\033[1;32mHID keycode pressed:  (0x%.2X) %s\033[0m\n",
+			       ch, keycode_to_string[ch]);
+		} else {
+			printf("\033[0;32mHID keycode released: (0x%.2X) %s\033[0m\n",
+			       ch, keycode_to_string[ch]);
+		}
+	} else if (msg[0] == MATRIX_EVENT) {
+		msg++;
+		uint8_t row = (uint8_t)msg[0] - 1;
+		uint8_t col = (uint8_t)msg[1] - 1;
+		bool pressed = (bool)msg[2];
 
-        if (pressed) {
-            printf("\033[1;34mMatrix event down:    (%d, %d)\033[0m\n",
-                   row, col);
-        } else {
-            printf("\033[0;34mMatrix event up:      (%d, %d)\033[0m\n",
-                   row, col);
-        }
-    }
+		if (pressed) {
+			printf("\033[1;34mMatrix event down:    (%d, %d)\033[0m\n",
+			       row, col);
+		} else {
+			printf("\033[0;34mMatrix event up:      (%d, %d)\033[0m\n",
+			       row, col);
+		}
+	}
 }
 
 void handle_daemon_message(char *msg)
 {
-    int i;
+	int i;
 
-    if (msg[0] == KEYCODE_HID) {
-        msg++;
-        uint8_t ch = (uint8_t)msg[0];
-        bool pressed = (bool)msg[1];
+	if (msg[0] == KEYCODE_HID) {
+		msg++;
+		uint8_t ch = (uint8_t)msg[0];
+		bool pressed = (bool)msg[1];
 
-        keys_down[ch] = pressed;
+		// remap mods for jack's keyboard
+		// would be nice to have this configurable somehow
+		// maybe a mapping of keycodes sent to the host?
 
-        uint8_t key_list[256] = { 0 };
-        uint8_t key_count = 0;
-
-        for (i = 0; i < 256; i++) {
-            if (keys_down[i]) {
-                key_list[key_count++] = i;
-            }
+        switch (ch) {
+        case KC_LCTL:
+            ch = KC_LGUI;
+            break;
+        case KC_LGUI:
+            ch = KC_LCTL;
+            break;
+        default:
+            break;
         }
 
-        int x;
-        unsigned char buf[16];
-        buf[0] = 1; // report id
-        buf[1] = 0; // mods
-        buf[2] = 0; // padding
-        for (x = 3; (x < 16); x++) {
-            buf[x] = key_list[x - 3]; // keycodes
+        switch (ch) {
+        case 0xE0 ... 0xE7:
+            if (pressed)
+                mods |= (1 << (ch & 0x7));
+            else
+                mods &= ~(1 << (ch & 0x7));
+            break;
+        default:
+            keys_down[ch] = pressed;
+            break;
         }
 
-        gadget_write(buf);
-    }
+
+		uint8_t key_list[256] = { 0 };
+		uint8_t key_count = 0;
+
+		for (i = 0; i < 256; i++) {
+			if (keys_down[i]) {
+				key_list[key_count++] = i;
+			}
+		}
+
+		int x;
+		unsigned char buf[16];
+		buf[0] = 1; // report id
+		buf[1] = mods; // mods
+		buf[2] = 0; // padding
+		for (x = 3; (x < 16); x++) {
+			buf[x] = key_list[x - 3]; // keycodes
+		}
+        printf("%.16X", buf);
+		gadget_write(buf);
+	}
 }
 
 struct qmk_gadget_cfg cfg = {
@@ -133,10 +161,10 @@ struct qmk_gadget_cfg cfg = {
 		},
 };
 
-static void __attribute__((noreturn)) usage(char * name)
+static void __attribute__((noreturn)) usage(char *name)
 {
-    fprintf(stderr, "Usage: %s [-hdoct]\n", name);
-    exit(EXIT_FAILURE);
+	fprintf(stderr, "Usage: %s [-hdoct]\n", name);
+	exit(EXIT_FAILURE);
 }
 
 int main(int argc, char *argv[])
@@ -157,7 +185,7 @@ int main(int argc, char *argv[])
 				read_message(nls, handle_daemon_message);
 			}
 			close(nls);
-            exit(EXIT_SUCCESS);
+			exit(EXIT_SUCCESS);
 			break;
 		case 'o':
 			exit(gadget_open("g1", &cfg));
@@ -167,25 +195,22 @@ int main(int argc, char *argv[])
 			break;
 		case 't':
 			send_test();
-            exit(EXIT_SUCCESS);
+			exit(EXIT_SUCCESS);
 			break;
 		default:
-            usage(argv[0]);
-            break;
+			usage(argv[0]);
+			break;
 		}
 	}
 
-    // initscr();
-    // cbreak();
-    // noecho();
-    nls = open_netlink();
-    send_message(nls, "hi!");
-    while (sig_flag) {
-        read_message(nls, handle_message);
-        // refresh();
-    }
-    close(nls);
-    // endwin();
+	nls = open_netlink();
+	send_message(nls, "hi!");
+	while (sig_flag) {
+		read_message(nls, handle_message);
+		// refresh();
+	}
+	close(nls);
+	// endwin();
 
 	return EXIT_SUCCESS;
 }
--- lib/libqmk/include/qmk/types.h
+++ lib/libqmk/include/qmk/types.h
@@ -32,6 +32,7 @@ struct qmk_keyboard {
 	uint8_t rows;
 	uint8_t cols;
 	qmk_keycode_t *keymap;
+	bool *key_state;
 	uint16_t layer_state;
 	uint8_t active_layer;
 	uint16_t mod_tap_timeout;
